4611e5fd8cd8c5ffa922d9ddac5580c49ed63604
Roy <rodrigo_a92@hotmail.com>
2021-08-13 11:11:33 -0300

    Correcciones expresion regular y validaciones

--- js/app.js
+++ js/app.js
@@ -7,25 +7,24 @@ const $tagsMessage = d.querySelector(".tags-message");
 
 //Añado el evento "keydown" al input, si el usuario presiona la tecla Espacio o Enter ejecuto la función createTag y le mando el valor sin espacios
 $input.addEventListener("keyup",(e)=>{
-  let regexp = /[a-zA-Z0-9ñÑ]/;
+  let regexp = /^[A-Za-z0-9Ññ]+$/;
 
-  if(!e.key.match(regexp) || ($input.value.length == 30 && e.keyCode !== 8)){
-    e.preventDefault();
-    $tagsMessage.style.display = "block";
-    
-    setTimeout(() => {
-      $tagsMessage.style.display = "none";
-    }, 5000);
-  }else{
-    $tagsMessage.style.display = "none";
-  }
-
-  if((e.keyCode == 32 || e.keyCode == 13) && $input.value !== ""){
+  if((e.keyCode == 32 || e.keyCode == 13) && $input.value.slice(0, -1).match(regexp)){
+    console.log("Condicional 1");
     $tagsMessage.style.display = "none";
     let value = e.target.value;
     createTag(value.trim());
     $input.value = null;
   }
+  
+  if($input.value !==""){
+    return !regexp.exec($input.value)
+      ? $tagsMessage.style.display = "block"
+      : $tagsMessage.style.display = "none"
+  }else{
+    $tagsMessage.style.display = "none"
+  }
+
 })
 
 //Genero el span y el botón para eliminar la etiqueta, le añado un eventListener al click y ejecuto la función deleteTag
